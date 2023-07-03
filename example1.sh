480aa4b4715448ea80333495c182cffd3d832171
EC2 Default User <ec2-user@ip-172-31-18-163.us-west-1.compute.internal>
2023-07-03 01:53:41 +0000

    added all my shellscripts

---
+++ example1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+var1="abc"
+var2="bengaluru"
+var3="9740417745"
+
+
+echo "this is $var1  and iam from $var2"
+
+echo "my cell phone number is $var3"
---
+++ example2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "$*"
+
+echo "this is $1  and iam from $2"
+
+echo "my cell phone number is $3"
